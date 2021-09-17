694ec1abbb37a635f9e680d643a70a822939018c
Sota Masuda <sota.masuda@the-lionsdata.com>
2021-09-17 20:14:19 +0900

    add argument parsing

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ But this is exercise for learning go, so I don't use lib.
 ## Links
 
 - https://gobyexample.com/command-line-arguments
+- Slice length and capacity
+  - https://tour.golang.org/moretypes/11
+- https://gobyexample.com/variables
+- Arrays
+  - https://tour.golang.org/moretypes/6
--- main.go
+++ main.go
@@ -7,8 +7,30 @@ import (
 )
 
 func main() {
+	// parse args
+	if len(os.Args) <= 1 || len(os.Args) >= 4 {
+		fmt.Println("num of arguments is invalid")
+		fmt.Println("arg1 : -n [optional]")
+		fmt.Println("arg2 : file name [positional]")
+		return
+	}
+	args := os.Args[1:]
+	// - set n option flag
+	// print_line_numbers := false
+	// if len(args) == 2 && args[1] == "-n" {
+	// 	print_line_numbers = true
+	// }
+
+	// - set input file name
+	filename := ""
+	if len(args) == 2 {
+		filename = args[1]
+	} else {
+		filename = args[0]
+	}
+
 	// open file
-	f, err := os.Open("test.txt")
+	f, err := os.Open(filename)
 	defer f.Close()
 	if err != nil {
 		fmt.Println(err)
