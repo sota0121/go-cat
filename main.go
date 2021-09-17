4ade5e417fd6fd89e7980ff2cc46e5dc367d5c54
Sota Masuda <sota.masuda@the-lionsdata.com>
2021-09-17 19:41:43 +0900

    implemented gcat base(no opt)

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("cat")
+	// open file
+	f, err := os.Open("test.txt")
+	defer f.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// read lines
+	fr := bufio.NewScanner(f)
+	if err := fr.Err(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for fr.Scan() {
+		fmt.Println(fr.Text())
+	}
+
 }
